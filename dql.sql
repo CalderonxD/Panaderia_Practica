fba310652e285c84af109da966df55f1f8fc78c2
CalderonxD <santiagoc200508@gmail.com>
2024-10-17 17:01:30 -0500

    feat: :sparkles: Nuevas consultas

--- dql.sql
+++ dql.sql
@@ -1,28 +1,75 @@
---select
+--SELECT
 
---1
+--a
 select * from productos;
 
---2
+--b
 select * from ingredientes;
 
---3
+--c
 select * from ventas where ventas.fecha = "2024-10-01";
 
---4
+--d
 select * from clientes;
 
---5
+--e
 select * from empleados;
 
---6
+--f
 select * from orden_proveedor;
 
---7
+--g
 select * from ventas where ventas.total > "100";
 
 
---8
+--h
 SELECT * 
 FROM productos 
 WHERE id_tipo = (SELECT id_tipo FROM tipo WHERE nombre = 'pan');
+
+--i
+SELECT * FROM panaderia.proveedores;
+
+--j
+select * from orden_compra_ingrediente_a_proveedor where estado = 'pendiente';
+
+
+
+--JOIN
+
+--a
+select * 
+from ventas
+inner join ventas_productos on ventas.id_venta = ventas_productos.id_venta
+inner join productos on ventas_productos.id_producto = productos.id_producto;
+
+--b
+select productos.nombre as nombre_producto, ingredientes.nombre as nombre_ingrediente
+from productos
+inner join ingredientes_productos on productos.id_producto = ingredientes_productos.id_producto
+inner join ingredientes on ingredientes_productos.id_ingrediente = ingredientes.id_ingrediente;
+
+--c
+select * 
+from empleados
+inner join ventas on empleados.id_empleado = ventas.id_empleado;
+
+--d
+select ingredientes.nombre as nombre_ingrediente, proveedores.nombre as nombre_proveedor
+from ingredientes
+inner join ingredientes_orden on ingredientes.id_ingrediente = ingredientes_orden.id_ingrediente
+inner join orden_compra_ingrediente_a_proveedor on ingredientes_orden.id_orden = orden_compra_ingrediente_a_proveedor.id_orden
+inner join orden_proveedor on orden_compra_ingrediente_a_proveedor.id_orden = orden_proveedor.id_orden
+inner join proveedores on orden_proveedor.id_proveedor = proveedores.id_proveedor;
+
+--e
+select *
+from clientes
+inner join ventas on clientes.id_cliente = ventas.id_cliente;
+
+
+--f
+select orden_compra_ingrediente_a_proveedor.*, proveedores.nombre as nombre_proveedor
+from orden_compra_ingrediente_a_proveedor
+inner join orden_proveedor on orden_compra_ingrediente_a_proveedor.id_orden = orden_proveedor.id_orden
+inner join proveedores on orden_proveedor.id_proveedor = proveedores.id_proveedor;
